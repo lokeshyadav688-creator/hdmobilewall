72eeec134253fa800328fc060e37ec195862daf8
lokeshyadav688-creator <lokeshyadav688@gmail.com>
2026-05-30 21:11:45 +0530

    Implement category filter for card display
    Added category filter functionality to the card display.

--- script.js
+++ script.js
@@ -27,3 +27,61 @@ document.getElementById("popup-img").src=src;
 function closeImage(){
 document.getElementById("popup").style.display="none";
 }
+const search = document.getElementById("search");
+
+search.addEventListener("keyup", function(){
+
+let value = search.value.toLowerCase();
+
+let cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+
+let text = card.innerText.toLowerCase();
+
+if(text.includes(value)){
+card.style.display="block";
+}else{
+card.style.display="none";
+}
+
+});
+
+});
+
+
+// Full Screen Preview
+function openImage(src){
+document.getElementById("popup").style.display="flex";
+document.getElementById("popup-img").src=src;
+}
+
+function closeImage(){
+document.getElementById("popup").style.display="none";
+}
+
+
+// Category Filter
+function filterSelection(category){
+
+let cards=document.querySelectorAll(".card");
+
+cards.forEach(card=>{
+
+if(category==="all"){
+card.style.display="block";
+}
+else{
+
+if(card.dataset.category===category){
+card.style.display="block";
+}
+else{
+card.style.display="none";
+}
+
+}
+
+});
+
+}
