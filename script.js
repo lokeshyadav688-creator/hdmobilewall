b494df58d3ce3c6ce3f9bc07a65092fa359e4a4f
lokeshyadav688-creator <lokeshyadav688@gmail.com>
2026-05-30 20:58:08 +0530

    Implement image popup functionality
    Added functions to open and close image popups.

--- script.js
+++ script.js
@@ -19,3 +19,11 @@ card.style.display="none";
 });
 
 });
+function openImage(src){
+document.getElementById("popup").style.display="flex";
+document.getElementById("popup-img").src=src;
+}
+
+function closeImage(){
+document.getElementById("popup").style.display="none";
+}
