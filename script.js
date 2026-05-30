6c7f0fcb2d09ca920a0bccd67be188fcec3263be
lokeshyadav688-creator <lokeshyadav688@gmail.com>
2026-05-30 17:06:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const search = document.getElementById("search");
+
+search.addEventListener("keyup", function(){
+
+let value = search.value.toLowerCase();
+
+let cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+
+let text = card.innerText.toLowerCase();
+
+if(text.includes(value)){
+card.style.display="block";
+}else{
+card.style.display="none";
+}
+
+});
+
+});
